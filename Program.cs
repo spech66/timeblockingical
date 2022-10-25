ab69c133fb179543a0227674835df23b5f1357b5
Sebastian Pech <spech@spech.de>
2022-10-25 20:11:49 +0200

    Initial

---
+++ Program.cs
@@ -0,0 +1,32 @@
+using Ical.Net;
+using Ical.Net.CalendarComponents;
+using Ical.Net.DataTypes;
+using Ical.Net.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+var now = DateTime.Now;
+var later = now.AddHours(1);
+
+// Repeat daily for 5 days
+var rrule = new RecurrencePattern(FrequencyType.Daily, 1) { Count = 5 };
+var e = new CalendarEvent
+{
+    Start = new CalDateTime(now),
+    End = new CalDateTime(later),
+    RecurrenceRules = new List<RecurrencePattern> { rrule },
+};
+
+var calendar = new Calendar();
+calendar.Events.Add(e);
+
+var serializer = new CalendarSerializer();
+var serializedCalendar = serializer.SerializeToString(calendar);
+
+app.MapGet("/", () =>
+{
+    return Results.Content(serializedCalendar, contentType: "text/calendar");
+});
+
+app.Run();
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# timeblockingical
-Timeblocking example using C# .NET Minimal API and ical.net
+# Timeblocking iCal
+
+Timeblocking example using C# .NET Minimal API and ical.net.
+
+Run using F5.
+
+The application will start and download the [5 day example](https://github.com/rianjs/ical.net/wiki) as iCal File.
